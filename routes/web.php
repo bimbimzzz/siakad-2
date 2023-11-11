c518be10e60949bb7342e8c56ffbad6047a4fba2
Kresna Bimantara <kresno.bimantoro@gmail.com>
2023-11-12 02:06:17 +0700

    membuat dashboard page

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.app.dashboard-siakad', ['type_menu' => '']);
+// Route::get('/', function () {
+//     return view('pages.app.dashboard-siakad', ['type_menu' => '']);
+// });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function(){
+        return view('pages.app.dashboard-siakad', ['type_menu' => '']);
+    })->name('home');
 });
 
-Route::get('/login', function () {
-    return view('pages.auth.auth-login');
-})->name('login');
+// Route::get('/login', function () {
+//     return view('pages.auth.auth-login');
+// })->name('login');
 
-Route::get('/register', function () {
-    return view('pages.auth.auth-register');
-})->name('register');
+// Route::get('/register', function () {
+//     return view('pages.auth.auth-register');
+// })->name('register');
 
-Route::get('/forgot', function () {
-    return view('pages.auth.auth-forgot-password');
-})->name('forgot');
+// Route::get('/forgot', function () {
+//     return view('pages.auth.auth-forgot-password');
+// })->name('forgot');
 
-Route::get('/reset', function () {
-    return view('pages.auth.auth-reset-password');
-})->name('reset');
+// Route::get('/reset', function () {
+//     return view('pages.auth.auth-reset-password');
+// })->name('reset');

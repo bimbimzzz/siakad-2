525e3b3a2fb1466c0cd4c59fd1d9c3b899a088c2
Kresna Bimantara <kresno.bimantoro@gmail.com>
2023-11-12 02:41:03 +0700

    logout

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('pages.app.dashboard-siakad', ['type_menu' => '']);
-// });
+Route::get('/', function () {
+    return view('pages.auth.auth-login');
+});
 
 Route::middleware(['auth'])->group(function () {
     Route::get('home', function(){
